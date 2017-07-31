deddb9694724ce7a7c89a5be8276de2939109052
Markus Ghambari <markus.ghambari@gmail.com>
2017-08-01 04:05:39 +1000

    Update common.js

--- code/common.js
+++ code/common.js
@@ -1,3 +1,4 @@
 document.body.onload= function() {
-	steem.config.set('websocket','wss://steemd.steemitdev.com');
+	//steem.config.set('websocket','wss://steemd.steemitdev.com');
+	steem.config.set('websocket','wss://steemd.steemit.com');
 }
