f3bb9859335ec26fb652bc5a51dd11db65375243
Markus Ghambari <markus.ghambari@gmail.com>
2017-08-01 12:27:13 +1000

    Update common.js

--- code/common.js
+++ code/common.js
@@ -1,6 +1,6 @@
 window.onload= function() {	
-   //steem.config.set('websocket', 'wss://steemd.steemit.com');
-   steem.config.set('websocket', 'wss://steemd.steemitdev.com');
+   steem.config.set('websocket', 'wss://steemd.steemit.com');
+   //steem.config.set('websocket', 'wss://steemd.steemitdev.com');
    //steem.config.set('websocket', 'wss://gtg.steem.house:8090');
    //steem.config.set('websocket', 'wss://seed.bitcoiner.me');
    //steem.config.set('websocket', 'wss://this.piston.rocks');
