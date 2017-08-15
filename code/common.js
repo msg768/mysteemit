098844045e5ed2e054f73152d827db8fc90f74bb
Markus Ghambari <markus.ghambari@gmail.com>
2017-08-16 00:38:27 +1000

    Update common.js

--- code/common.js
+++ code/common.js
@@ -1,10 +1,12 @@
-window.onload = function() {	
-	var link = 'https://rawgit.com/msg768/mysteemit/master/';		
-	if(!window.location.href.startsWith(link)) {
-		window.location.href = link+app+'.html';
-	} else {
-		if(app == 'raffle') { start(); }
-	}
+window.onload = function () {
+   var link = 'https://rawgit.com/msg768/mysteemit/master/';
+   if (!window.location.href.startsWith(link)) {
+      //window.location.href = link+app+'.html';
+   } else {
+      if (app == 'raffle') {
+         start();
+      }
+   }
 
    //steem.config.set('websocket', 'wss://steemd.steemit.com');
    //steem.config.set('websocket', 'wss://steemd.steemitdev.com');
@@ -14,9 +16,9 @@ window.onload = function() {
    //steem.config.set('websocket', 'wss://node.steem.ws');
 }
 
-Number.prototype.zeroPad = function(length) {
+Number.prototype.zeroPad = function (length) {
    length = length || 2; // defaults to 2 if no parameter is passed
-   return (new Array(length).join('0')+this).slice(length*-1);
+   return (new Array(length).join('0') + this).slice(length * -1);
 };
 
 Date.prototype.getUTCTime = function () {
@@ -24,14 +26,14 @@ Date.prototype.getUTCTime = function () {
 };
 
 Date.prototype.getUTCString = function () {
-	return this.getUTCFullYear() + '-' + (this.getUTCMonth()+1).zeroPad(2) + '-' + (this.getUTCDate()).zeroPad(2) + 'T' + (this.getUTCHours()).zeroPad(2) + ':' + (this.getUTCMinutes()).zeroPad(2) + ':' + (this.getUTCSeconds()).zeroPad(2);
+   return this.getUTCFullYear() + '-' + (this.getUTCMonth() + 1).zeroPad(2) + '-' + (this.getUTCDate()).zeroPad(2) + 'T' + (this.getUTCHours()).zeroPad(2) + ':' + (this.getUTCMinutes()).zeroPad(2) + ':' + (this.getUTCSeconds()).zeroPad(2);
 };
 
 Date.prototype.getSteemString = function () {
-	return this.getFullYear() + '-' + (this.getMonth()+1).zeroPad(2) + '-' + (this.getDate()).zeroPad(2) + 'T' + (this.getHours()).zeroPad(2) + ':' + (this.getMinutes()).zeroPad(2) + ':' + (this.getSeconds()).zeroPad(2);
+   return this.getFullYear() + '-' + (this.getMonth() + 1).zeroPad(2) + '-' + (this.getDate()).zeroPad(2) + 'T' + (this.getHours()).zeroPad(2) + ':' + (this.getMinutes()).zeroPad(2) + ':' + (this.getSeconds()).zeroPad(2);
 };
 
 function customRand(seed) {
-    var x = Math.sin(seed) * 10000;
-    return x - Math.floor(x);
+   var x = Math.sin(seed) * 10000;
+   return x - Math.floor(x);
 }
