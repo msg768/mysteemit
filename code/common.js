61ff17f2132f4f1f0a8d790f4dadec1215114d47
Markus GH <markus.ghambari@gmail.com>
2018-01-13 13:49:37 +1100

    Update common.js

--- code/common.js
+++ code/common.js
@@ -9,12 +9,7 @@ window.onload = function () {
       }
    }
 
-   //steem.config.set('websocket', 'wss://steemd.steemit.com');
-   //steem.config.set('websocket', 'wss://steemd.steemitdev.com');
-   //steem.config.set('websocket', 'wss://gtg.steem.house:8090');
-   //steem.config.set('websocket', 'wss://seed.bitcoiner.me');
-   //steem.config.set('websocket', 'wss://this.piston.rocks');
-   //steem.config.set('websocket', 'wss://node.steem.ws');
+   steem.api.setOptions({ url: 'https://api.steemit.com' });
 }
 
 Number.prototype.zeroPad = function (length) {
